26e5682cf947bade6f4d7e9136841894f7d11138
Ishu Bansal <ishubansal1400@gmail.com>
2025-02-14 21:21:47 +0530

    add default to timestamp

--- models/expense.js
+++ models/expense.js
@@ -4,7 +4,7 @@ const { CATEGORY, PAYMENT_MODE, RECIPIENT } = require('../constants');
 const ExpenseSchema = new mongoose.Schema({
     title: { type: String, required: true },
     amount: { type: Number, required: true },
-    timestamp: { type: Date, required: true },
+    timestamp: { type: Date, required: true, default: Date.now },
     category: { type: String, required: true, enum: Object.values(CATEGORY) },
     paymentMode: { type: String, required: true, enum: Object.values(PAYMENT_MODE) },
     recipient: { type: String, required: true, enum: Object.values(RECIPIENT) },
